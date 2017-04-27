aeed1877426226dd2eb1592e459fabe5af778b8a
callumjhays <callumjhays@gmail.com>
2017-04-27 10:04:18 -0700

    make docs elm-package compliant

--- src/Unwrap.elm
+++ src/Unwrap.elm
@@ -6,6 +6,9 @@ and time again, so I thought to separate it out into a package.
 
 Only use these if the program SHOULD crash in the case of the value not being
 present.
+
+# functions
+@docs maybe, result
 -}
 
 
